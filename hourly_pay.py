5a4bcb0ad8b021bc4de81e13690c196913cb63c7
Justin Tyler Rackley <justinrackley1337@gmail.com>
2020-04-23 18:18:20 -0400

    added overtime pay

--- hourly_pay.py
+++ hourly_pay.py
@@ -1,4 +1,10 @@
 hrs = int(input("Enter Hours:"))
 rate = float(input("Enter Rate:"))
-pay = float(hrs*rate)
-print("Pay:", pay)
+if hrs > 40 :
+    regular_hours = hrs * rate
+    over_time = (hrs - 40.0) * (rate * 0.5)
+    pay = regular_hours + over_time
+    print(pay)
+elif hrs <= 40 :
+    no_overtime = hrs * rate
+    print(no_overtime)
